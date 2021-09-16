9bb67941bcf886219057780b3072b3c6085c05d8
Borodin239 <j.borodin1@yandex.ru>
2021-09-17 01:21:06 +0300

    Добавлены модули frontend, создание базовых компонент

---
+++ frontend/src/components/Middle.vue
@@ -0,0 +1,56 @@
+<template>
+  <html lang="en">
+  <head>
+
+    <!-- Loading bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <title>Аутентификация</title>
+  </head>
+
+  <body>
+  <div class="main-container row justify-content-center">
+    <div class="col-4 align-self-center">
+      <div class="card">
+        <div class="card-body">
+          <form method="post" style="margin: 10px">
+            <h2 style="margin-bottom: 30px">Аутентификация</h2>
+            <div class="mb-4">
+              <label for="username_input" class="form-label">Имя пользователя</label>
+              <input type="text" name="username_input" class="form-control" id="username_input">
+            </div>
+            <div class="mb-4">
+              <label for="password_input" class="form-label">Пароль</label>
+              <input type="password" name="password_input" class="form-control" id="">
+            </div>
+            <button type="submit" class="btn btn-outline-secondary">Войти</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  </body>
+  </html>
+
+</template>
+
+<script>
+export default {
+  name: "Middle"
+}
+</script>
+
+<style scoped>
+template {
+  height: max-content;
+}
+
+.main-container {
+  position: absolute;
+  height: 100%;
+  margin-bottom: calc(10vh + 5px);
+  width: 100%;
+  background-color: #323242;
+}
+</style>
