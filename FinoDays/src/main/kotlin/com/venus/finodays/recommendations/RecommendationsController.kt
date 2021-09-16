7d272e9ac207cae26f173e5c12f4663238805c17
DenisKorotchenko <dzd1024ozd@gmail.com>
2021-09-16 21:57:10 +0300

    Add Categories handle

---
+++ FinoDays/src/main/kotlin/com/venus/finodays/recommendations/RecommendationsController.kt
@@ -0,0 +1,23 @@
+package com.venus.finodays.recommendations
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/recommendations/")
+class RecommendationsController {
+
+    @Autowired
+    private lateinit var categoriesService: CategoriesService
+
+    @GetMapping("/top_categories/")
+    fun getTopCategories(
+        @RequestParam count: Int?
+    ): List<Category> {
+        return categoriesService.getTopCategories(count ?: 5)
+        // todo: нормальная обработка (deniskorotchenko)
+    }
+}
